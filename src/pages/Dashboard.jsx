ae84fc1ed23b15fa079303504d214a221d3d9e70
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-23 16:22:37 +0100

    Implémente affichage des dernières dépenses

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { auth, db } from "../../firebase";
+import { collection, limit, orderBy, query } from "firebase/firestore";
 
 import { StatusBar } from "expo-status-bar";
-import { auth } from "../../firebase";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const Dashboard = ({ navigation }) => {
+	const user = auth.currentUser;
+
+	const [depenses, loading, error] = useCollectionData(
+		query(
+			collection(db, "users", user.uid, "depenses"),
+			orderBy("date", "desc"),
+			limit(4)
+		)
+	);
+
+	const renderDepense = ({ item }) => (
+		<View
+			style={{
+				flexDirection: "row",
+				justifyContent: "space-between",
+				width: 300,
+			}}>
+			<Text style={styles.depense}>{item.nom}</Text>
+			<Text style={styles.depense}>{item.montant} €</Text>
+		</View>
+	);
+
 	return (
 		<View style={styles.container}>
-			<Text>Dashboard</Text>
-			<Text>{auth.currentUser?.uid}</Text>
+			<Text style={styles.title}>Dernières dépenses</Text>
+			{depenses && (
+				<FlatList
+					style={styles.depenses}
+					data={depenses}
+					renderItem={renderDepense}
+					keyExtractor={(item) => item.nom}
+				/>
+			)}
+			{loading && <Text>Chargement de vos dernières dépenses...</Text>}
+			{error && <Text>Erreur : {JSON.stringify(error)}</Text>}
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -17,9 +50,24 @@ export default Dashboard;
 
 const styles = StyleSheet.create({
 	container: {
+		padding: 30,
 		flex: 1,
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
+	},
+
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		alignSelf: "flex-start",
+	},
+
+	depenses: {
+		marginTop: 10,
+	},
+
+	depense: {
+		fontSize: 16,
 	},
 });
