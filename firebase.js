9150f17a5d282b98c1a706d1ca6914fa6c10d04b
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-23 10:57:36 +0100

    Add firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDYtDHbBmWAjUBGbFV7mwA3be5yofjkwlM",
+	authDomain: "okane-73ae0.firebaseapp.com",
+	projectId: "okane-73ae0",
+	storageBucket: "okane-73ae0.appspot.com",
+	messagingSenderId: "705424858248",
+	appId: "1:705424858248:web:0ed7c800b85ffcafc6133d",
+	measurementId: "G-HTLPNTEQ53",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
