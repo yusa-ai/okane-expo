09c8489853eb294122cebc674bfb102ab3cec6f4
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-23 16:52:29 +0100

    Add remaining tabs (placeholders)

--- src/routes/AuthenticatedTab.jsx
+++ src/routes/AuthenticatedTab.jsx
@@ -1,4 +1,4 @@
-import Dashboard from "../pages/Dashboard";
+import Accueil from "../pages/Accueil";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -9,11 +9,51 @@ const AuthenticatedTab = () => {
 		<Tab.Navigator>
 			<Tab.Screen
 				name="Accueil"
-				component={Dashboard}
+				component={Accueil}
 				options={{
-					tabBarIcon: (
-						{ color, size } // TODO focused
-					) => <Ionicons name="home" color={color} size={size} />,
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="home" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Dépenses"
+				component={Accueil}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="cash-outline" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Nouvelle dépense"
+				component={Accueil}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="add-circle-outline" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Conseils"
+				component={Accueil}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name="chatbubble-ellipses-outline"
+							color={color}
+							size={size}
+						/>
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Profil"
+				component={Accueil}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="person-circle-outline" color={color} size={size} />
+					),
 				}}
 			/>
 		</Tab.Navigator>
